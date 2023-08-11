264123ac6f87a0b9a8adeef7ac38955162ae35b3
SaiSugunSegu <snaimesha@gmail.com>
2023-08-11 17:03:27 +0530

    ros dev

--- src/slam.py
+++ src/slam.py
@@ -2,9 +2,13 @@
 
 import rospy
 from std_msgs.msg import String 
+from sensor_msgs.msg import Image
+import matplotlib.pyplot as plt
+import message_filters
 import sys
+import numpy as np
 sys.path
-sys.path.append('/work/repos/LightGlue')
+sys.path.append('/home/su/work/repos/LightGlue')
 
 from pathlib import Path
 from lightglue import LightGlue, SuperPoint, DISK
@@ -14,11 +18,27 @@ import torch
 torch.set_grad_enabled(False);
 
 
-def callback(data):
-    print("call")
+def numpy_image_to_torch(image: np.ndarray) -> torch.Tensor:
+    """Normalize the image tensor and reorder the dimensions."""
+    if image.ndim == 3:
+        image = image.transpose((2, 0, 1))  # HxWxC to CxHxW
+    elif image.ndim == 2:
+        image = image[None]  # add channel axis
+    else:
+        raise ValueError(f'Not an image: {image.shape}')
+    return torch.tensor(image / 255., dtype=torch.float)
+
+def inter_matcher(image):
+    print("")
+
+def match_callback(image0, image1):
+
+    image0 = np.frombuffer(image0.data, dtype=np.uint8).reshape(image0.height, image0.width, -1)
+    image1 = np.frombuffer(image1.data, dtype=np.uint8).reshape(image1.height, image1.width, -1)
+
     # load images
-    image0 = load_image('/work/repos/LightGlue/assets/DSC_0410.JPG')
-    image1 = load_image('/work/repos/LightGlue/assets/DSC_0410.JPG')
+    image0 = numpy_image_to_torch(image0).cuda()
+    image1 = numpy_image_to_torch(image1).cuda()
 
     # extract features using 'Super Point' with max_num_key_point
     feats0 = extractor.extract(image0.to(device))
@@ -35,19 +55,36 @@ def callback(data):
     m_kpts0, m_kpts1 = kpts0[matches[..., 0]], kpts1[matches[..., 1]]
     print("Match Points", m_kpts0.shape[0])
 
-    
+    axes = viz2d.plot_images([image0, image1])
+    viz2d.plot_matches(m_kpts0, m_kpts1, color='lime', lw=0.2)
+    viz2d.add_text(0, f'Stop after {matches01["stop"]} layers', fs=20)
+
+    # plt.imshow(image0)
+
     
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/chatter", String, callback)
-    print("in node")
+
+    # rospy.Subscriber("/cam_front/raw", Image, inter_matcher)
+    # rospy.Subscriber("/cam_front_right/raw", Image, inter_matcher)
+    # rospy.Subscriber("/cam_front_left/raw", Image, inter_matcher)
+
+    front_sub = message_filters.Subscriber("/cam_front/raw", Image)
+    right_sub = message_filters.Subscriber("/cam_front_right/raw", Image)
+    left_sub = message_filters.Subscriber("/cam_front_left/raw", Image)
+
+    front_right = message_filters.ApproximateTimeSynchronizer([front_sub, right_sub], 10, 0.1, allow_headerless=True)
+    front_right.registerCallback(match_callback)
+
+    front_left = message_filters.ApproximateTimeSynchronizer([front_sub, left_sub], 10, 0.1, allow_headerless=True)
+    front_left.registerCallback(match_callback)
+
     rospy.spin()
 
 if __name__ == '__main__':
-    print("in main")
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # 'mps', 'cpu'
-    extractor = SuperPoint(max_num_keypoints=512).eval().to(device)  # load the extractor
-    matcher = LightGlue(features='superpoint').eval().to(device)
+    extractor = SuperPoint(max_num_keypoints=2048).eval().to(device).cuda()  # load the extractor
+    matcher = LightGlue(features='superpoint').eval().to(device).cuda()
 
     listener()
